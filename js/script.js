a0bfc6936cd88331f2aa2c1fb471043c10647fa2
beatriz_g <beatrizgue20@gmail.com>
2023-03-10 17:22:47 -0800

    Added innerText calculation

--- js/script.js
+++ js/script.js
@@ -15,3 +15,16 @@ subDurationElement.addEventListener("change", function(e) {
     subDurationElement = Number(e.target.value);
     console.log(subDuration);
 });
+
+//calculate the cost
+var updateSubscriptionDiv = function() {
+    var monthlyCost = 5;
+    if (subtype === "standard") {
+        monthlyCost = 7;
+    } else if (subtype === "premium") {
+        monthlyCost = 10;
+    }
+
+    var total = subduration * monthlyCost;
+    result.innerText = `You have chosen a ${subDuration} month ${subType} plan for $${total}.`
+};
