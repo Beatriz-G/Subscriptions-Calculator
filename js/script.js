e86bd84c710bb71fa67c6c665a4a2f3d33d3e593
beatriz_g <beatrizgue20@gmail.com>
2023-03-10 16:54:53 -0800

    Added elements

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
 // Your JS code here
+//elements
+var subTypeElement = document.querySelector(".subscription-select");
+var subDurationElement = document.querySelector(".duration-select");
+var result = document.querySelector("result");
+var subtype = document.querySelector("basic");
+var subDuration = Number("1");
