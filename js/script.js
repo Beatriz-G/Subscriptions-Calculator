52879c97c0c7f4c8cdf0cc67af2b435aacb20c19
beatriz_g <beatrizgue20@gmail.com>
2023-03-10 17:29:06 -0800

    Added call function to dom

--- js/script.js
+++ js/script.js
@@ -3,28 +3,34 @@
 var subTypeElement = document.querySelector("#subscription");
 var subDurationElement = document.querySelector("#months");
 var result = document.querySelector(".result");
-var subtype = "basic";
+var subType = "basic";
 var subDuration = 1;
 
 subTypeElement.addEventListener("change", function(e) {
     subType = e.target.value;
     //console.log(subtype);
+
+    
+    updateSubscriptionDiv();
 });
 
 subDurationElement.addEventListener("change", function(e) {
     subDurationElement = Number(e.target.value);
-    console.log(subDuration);
+    //console.log(subDuration);
+
+    
+    updateSubscriptionDiv();
 });
 
 //calculate the cost
 var updateSubscriptionDiv = function() {
     var monthlyCost = 5;
-    if (subtype === "standard") {
+    if (subType === "standard") {
         monthlyCost = 7;
-    } else if (subtype === "premium") {
+    } else if (subType === "premium") {
         monthlyCost = 10;
     }
 
-    var total = subduration * monthlyCost;
+    var total = subDuration * monthlyCost;
     result.innerText = `You have chosen a ${subDuration} month ${subType} plan for $${total}.`
 };
